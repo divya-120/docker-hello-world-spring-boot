2121bf3e16b9306163845633146571b350b85ecf
divya-120 <57285891+divya-120@users.noreply.github.com>
2022-01-11 15:10:02 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World\n";
+        return "I love India ,India is great\n";
     }
     
 }
